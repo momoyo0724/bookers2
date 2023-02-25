b605cfb9b1ee0c7991da1b3b768d6e4454ae7d4e
momoyo0724 <122843585+momoyo0724@users.noreply.github.com>
2023-02-25 02:51:54 +0000

    2023-02-25-start

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+
+  resources :lists, only: [:new, :index, :show]
+
+  get 'homes/top'
+  devise_for :users
+  root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
